afd1c95bfd4454ae38d8ee9ec0657e799a9ceeaa
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2017-03-21 11:32:44 -0600

    Implemento updateUser.js con soporte en una única tabla para datos de usuario, contacto y escuela

--- routes/users/me.post.js
+++ routes/users/me.post.js
@@ -5,7 +5,7 @@ module.exports = {
     path: '/users/me',
     method: 'POST',
     handler(request, reply) {
-        updateUser(request.auth.credentials, request.payload, reply);
+        updateUser(request.getDb().getModel('Usuario'), request.auth.credentials, request.payload, reply);
     },
     config: {
         description: 'Actualizar perfil del usuario actual',
@@ -19,10 +19,24 @@ module.exports = {
                 email: Joi.string().label('Correo electrónico').example('nombre@example.org')
                     .email()
                     .lowercase(),
+                CURP: Joi.string().label('CURP').example('BADD110313HCMLNS09')
+                    .uppercase()
+                    .length(18),
+                rolEscuela: Joi.string().label('Rol en la escuela').example('participante')
+                    .lowercase()
+                    .max(31),
                 telefono: Joi.number().label('Teléfono de contacto').example('4421111222')
                     .integer()
                     .min(1000000000)
-                    .max(9999999999)
+                    .max(9999999999),
+                claveEscuela: Joi.string().label('Clave de escuela').example('22DPR0129A')
+                    .uppercase()
+                    .length(10),
+                nombreEscuela: Joi.string().label('Nombre de la escuela del usuario').example('Enrique Barrera')
+                    .max(255),
+                regionEscuela: Joi.string().label('Región donde se encuentra la escuela del usuario').example('cadereyta')
+                    .lowercase()
+                    .max(31)
             }
         }
     }
