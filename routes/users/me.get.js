b161b23753cc1faf482efc7ce6fecdced9f28768
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2017-03-27 02:01:47 -0600

    signJwt: abstraigo el módulo para firmar jwt passwordless: uso signJwt. me.get, findOrcreateUser: uso signJwt para firmar tokens con uuid y examenId

--- routes/users/me.get.js
+++ routes/users/me.get.js
@@ -1,10 +1,15 @@
+const signJwt = require('../../lib/signJwt');
 const findOrCreateUser = require('../../lib/findOrCreateUser');
 
 module.exports = {
     path: '/users/me',
     method: 'GET',
     handler(request, reply) {
-        findOrCreateUser(request.getDb().getModel('Usuario'), request.auth.credentials, reply);
+        findOrCreateUser(request.getDb().getModel('Usuario'), request.auth.credentials)
+        .then(
+            usuario => reply({ usuario, jwt: signJwt(usuario.email, usuario.uuid) }),
+            boom => reply(boom)
+        );
     },
     config: {
         auth: 'jwt',
--- routes/users/passwordless.js
+++ routes/users/passwordless.js
@@ -1,22 +1,13 @@
 const Joi = require('joi');
-const JWT = require('jsonwebtoken');
-const config = require('../../config.js');
+const signJwt = require('../../lib/signJwt');
 const emailToken = require('../../lib/emailToken');
-
-const TOKEN_TTL = '2h';
 
 module.exports = {
     path: '/users/passwordless',
     method: 'POST',
     handler(request, reply) {
-        const options = {
-            expiresIn: TOKEN_TTL
-        };
-        const session = {
-            id: 'otp',
-            email: request.payload.email
-        };
-        const token = JWT.sign(session, config.authKey, options);
+        const token = signJwt(request.payload.email);
+        console.log(token);
         emailToken(token, request.payload, reply);
     },
     config: {
